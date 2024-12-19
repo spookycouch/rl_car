c5a9e2abbcc3926a034b35f228253f99c67308d6
Joe Jeffcock <tp21629@bristol.ac.uk>
2024-12-19 00:40:13 +0000

    feat: set up train to use hydra configs

--- python/train.py
+++ python/train.py
@@ -1,14 +1,46 @@
-import time
-from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
-from car import CarEnv
+import argparse
+from datetime import datetime
 
-MODEL_NAME = "ppo_car"
+from dataclasses import dataclass
+import os
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+from stable_baselines3.common.base_class import BaseAlgorithm
 
-parameters = CarEnv.Parameters(real_time=False)
-env = CarEnv(parameters)
-check_env(env)
+MODELS_SAVE_DIR = "./models"
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=1e5)
-model.save(MODEL_NAME)
+@dataclass
+class TrainingParameters:
+    save_prefix: str
+    total_timesteps: int
+
+def get_model_save_path(
+    save_prefix: str,
+    training_start_time: datetime,
+):
+    training_start_time_str = training_start_time.strftime('%Y-%m-%dT%H:%M:%S')
+    model_save_name = f"{save_prefix}_{training_start_time_str}"
+    model_save_path = os.path.join(MODELS_SAVE_DIR, model_save_name)
+    return model_save_path
+
+def main(cfg: DictConfig):
+    training_start_time = datetime.now()
+    model: BaseAlgorithm = instantiate(cfg.model)
+    training_params: TrainingParameters = instantiate(cfg.training_parameters)
+    
+    model.learn(total_timesteps=training_params.total_timesteps)
+
+    model_save_path = get_model_save_path(training_params.save_prefix, training_start_time)
+    model.save(model_save_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train the RL car.")
+    parser.add_argument("config_path", type=str, help="Path to a hydra config for the RL task.")
+    args = parser.parse_args()
+
+    config_dir, config_name = os.path.split(args.config_path)
+
+    with initialize(config_path=config_dir):
+        cfg = compose(config_name=config_name)
+        main(cfg)
